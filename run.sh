95ed51e4d325f8875119b5fcb56cd49d2448295e
David Taylor <david.a.taylor@dpi.nsw.gov.au>
2022-03-08 10:43:11 +1100

    Added check for compose/.env to run.sh.

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 BROKER_ROOT=$(cd $(dirname $0); pwd)
 MODE=${1:-test}
 
+if [ ! -f $BROKER_ROOT/compose/.env ]; then
+    echo The file $BROKER_ROOT/compose/.env is missing. Copy $BROKER_ROOT/config/broker.env.template to $BROKER_ROOT/compose/.env and set the values.
+    exit 1
+fi
+
 cd $BROKER_ROOT
 cd compose/$MODE
 ./dc.sh stop
