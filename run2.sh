eb2cb5b99e3fb0b246ff7daf27f4d32598a6b586
cal <14177650+cufffs@users.noreply.github.com>
2023-03-20 00:14:23 +1100

    fixed run2.sh if no nginx container exists

--- run2.sh
+++ run2.sh
@@ -3,15 +3,19 @@
 CWD="$(pwd)"
 
 #add our nginx container stuff
-NGINX_ROOT=$(cd 303/nginx;pwd)
+NGINX_ROOT=$(
+	cd 303/nginx
+	pwd
+)
 cd $NGINX_ROOT
-echo "starting nginx container"
-docker stop nginx-t >> /dev/null
-docker rm nginx-t >> /dev/null
-docker rmi nginx_img >> /dev/null
+docker ps | grep -q "nginx-t" >>/dev/null && docker stop nginx-t >>/dev/null && echo "Stopping nginx-t"
+docker ps -a | grep -q "nginx-t" >>/dev/null && docker rm nginx-t >>/dev/null
+docker images -a | grep -q "nginx_img" >>/dev/null && docker rmi nginx_img >>/dev/null
+docker images -a | grep -q "nginx" || docker pull nginx && echo "pulling nginx image"
 docker build -q -t nginx_img .
-docker run --name nginx-t -p 80:80 -d nginx_img:latest >> /dev/null
-docker start nginx-t >> /dev/null
+docker run --name nginx-t -p 80:80 -d nginx_img:latest >>/dev/null
+echo "starting nginx container"
+docker start nginx-t >>/dev/null
 docker ps | grep -q 'nginx-t' && echo 'nginx-t started'
 
 #run the actual run
