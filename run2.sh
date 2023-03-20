6933f0c22488fb753263c17a2cf7df7765c90d39
callum <14177650+cufffs@users.noreply.github.com>
2023-03-20 11:04:21 +1100

    removed some unessary bits

--- run2.sh
+++ run2.sh
@@ -8,13 +8,12 @@ NGINX_ROOT=$(
 	pwd
 )
 cd $NGINX_ROOT
-docker ps | grep -q "nginx-t" >>/dev/null && echo "Stopping nginx-t" && docker stop nginx-t >>/dev/null
-docker ps -a | grep -q "nginx-t" >>/dev/null && docker rm nginx-t >>/dev/null
-docker images -a | grep -q "nginx_img" >>/dev/null && docker rmi nginx_img >>/dev/null
+docker ps | grep -q "nginx-t" echo "Stopping nginx-t" && docker stop nginx-t >/dev/null
+docker ps -a | grep -q "nginx-t" && docker rm nginx-t >/dev/null
+docker images -a | grep -q "nginx_img" && docker rmi nginx_img >/dev/null
 docker images -a | grep -q "nginx" || echo "pulling nginx image" && docker pull nginx:latest
 docker build -q -t nginx_img .
-docker run --name nginx-t -p 80:80 -d nginx_img:latest >>/dev/null
-echo "starting nginx container"
+docker run --name nginx-t -p 80:80 -d nginx_img:latest >/dev/null
 docker start nginx-t >>/dev/null
 docker ps | grep -q 'nginx-t' && echo 'nginx-t started'
 
