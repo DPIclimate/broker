a9cf9935c18a3502df43339d46806b109235adfd
cal <14177650+cufffs@users.noreply.github.com>
2023-04-10 10:47:01 +1000

    start/stop/testing scripts modified to be better

--- 303/iteration-2/competency-proof/stop.sh
+++ 303/iteration-2/competency-proof/stop.sh
@@ -4,3 +4,5 @@ echo 'stopping rabbitmq...'
 docker stop rabbitmq >/dev/null
 echo 'stopping questdb...'
 docker stop questdb >/dev/null
+echo 'stopping api...'
+docker stop api >/dev/null
--- 303/iteration-2/competency-proof/test.sh
+++ 303/iteration-2/competency-proof/test.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-python -m pytest -v
+#at the very least dbhost is required to run
+#as api container calls it and requires the ip,
+#the rest can just use localhost
+
+container_names=("questdb" "api" "rabbitmq")
+
+for container_name in "${container_names[@]}"; do
+	if [ "$(docker inspect -f '{{.State.Running}}' $container_name)" != "true" ]; then
+		echo "Error: $container_name is not running"
+		exit
+	fi
+done
+
+db=$(docker inspect questdb | grep -m 1 -oP '(?<=IPAddress": ")[^"]*')
+ap=$(docker inspect api | grep -m 1 -oP '(?<=IPAddress": ")[^"]*')
+mq=$(docker inspect rabbitmq | grep -m 1 -oP '(?<=IPAddress": ")[^"]*')
+
+python -m pytest -v --dbhost ${db} --aphost ${ap} --mqhost ${mq}
