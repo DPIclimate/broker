561477a29b5c12d64203653a6aa86fd78add85b2
Jack Footner <jackrfootner@gmail.com>
2023-04-24 14:01:23 +0930

    Initial Python File Setup for MQTT Processors

---
+++ src/python/mqtt_processors/MQTTProcessors.py
@@ -0,0 +1,99 @@
+import datetime, dateutil.parser
+
+import asyncio, json, logging, re, signal, sys, uuid
+from typing import Dict, Optional
+
+import BrokerConstants
+from pdmodels.Models import PhysicalDevice
+from pika.exchange_type import ExchangeType
+
+import api.client.RabbitMQ as mq
+import api.client.TTNAPI as ttn
+
+import api.client.DAO as dao
+
+import util.LoggingUtil as lu
+import util.Timestamps as ts
+
+std_logger = logging.getLogger(__name__)
+
+rx_channel: mq.RxChannel = None
+tx_channel: mq.TxChannel = None
+mq_client: mq.RabbitMQConnection = None
+finish = False
+
+def sigterm_handler(sig_no, stack_frame) -> None:
+    """
+    Handle SIGTERM from docker by closing the mq and db connections and setting a
+    flag to tell the main loop to exit.
+    """
+    global finish, mq_client
+
+    logging.debug(f'{signal.strsignal(sig_no)}, setting finish to True')
+    finish = True
+    dao.stop()
+    mq_client.stop()
+
+
+async def main():
+    """
+    Initiate the connection to RabbitMQ and then idle until asked to stop.
+
+    Because the messages from RabbitMQ arrive via async processing this function
+    has nothing to do after starting connection.
+
+    It would be good to find a better way to do nothing than the current loop.
+    """
+    global mq_client, rx_channel, tx_channel, finish
+
+    logging.info('===============================================================')
+    logging.info('               STARTING MQTT Processor LISTENER')
+    logging.info('===============================================================')
+
+    # The default MQTT topic of YDOC devices is YDOC/<serial#> which RabbitMQ converts into a routing key of YDOC.<serial#>.
+    # It seems we can use the MQTT topic wildcard of # to get all YDOC messages. 'YDOC.#'
+    # Wombat uses the topic of 'wombat'
+    
+    # TODO - Iterate over plugins and load each one
+
+    #while not (rx_channel.is_open and tx_channel.is_open):
+    while not (rx_channel.is_open):
+        await asyncio.sleep(0)
+
+    while not finish:
+        await asyncio.sleep(2)
+
+    while not mq_client.stopped:
+        await asyncio.sleep(1)
+
+
+def on_message(channel, method, properties, body):
+    """
+    This function is called when a message arrives from RabbitMQ.
+    """
+    global rx_channel, tx_channel, finish
+
+    delivery_tag = method.delivery_tag
+
+    # If the finish flag is set, reject the message so RabbitMQ will re-queue it
+    # and return early.
+    if finish:
+        rx_channel._channel.basic_reject(delivery_tag)
+        return
+
+    try:
+        # process message
+        logging.info("Message Received")
+    except Exception as e:
+        std_logger.exception('Error while processing message.')
+        rx_channel._channel.basic_ack(delivery_tag)
+
+
+if __name__ == '__main__':
+    # Docker sends SIGTERM to tell the process the container is stopping so set
+    # a handler to catch the signal and initiate an orderly shutdown.
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
+    # Does not return until SIGTERM is received.
+    asyncio.run(main())
+    logging.info('Exiting.')
