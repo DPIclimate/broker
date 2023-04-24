d44c82e3e643129bf740a661bd9c071b426ae10a
RishCSU <rsing107@postoffice.csu.edu.au>
2023-04-24 17:59:29 +1000

    various chnges

--- 303/elaboration-1/prom/start.sh
+++ 303/elaboration-1/prom/start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo 'starting docker container'
-docker run -d --rm --name prometheus -p 9090:9090 prom/prometheus > /dev/null
+python3 -m http.server &
+docker run -d --rm --name json_exporter -p 7979:7979 -v $PWD/config/config.yml:/config.yml quay.io/prometheuscommunity/json-exporter --config.file=/config.yml 
+docker run -d --rm --name prometheus -p 9090:9090 -v $PWD/config/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus 
 
 echo '---'
 
-docker ps | grep -q "prometheus" && echo 'prometheus running'
+docker ps | grep -q "prom/prometheus" && echo 'prometheus running'
+docker ps | grep -q "json_exporter" && echo 'json_exporter running'
+docker ps
--- 303/elaboration-1/prom/stop.sh
+++ 303/elaboration-1/prom/stop.sh
@@ -2,3 +2,5 @@
 
 echo 'stopping prometheus...'
 docker stop prometheus > /dev/null
+docker stop json_exporter > /dev/null
+kill -9 `ps | grep python3 | grep -Eo [0-9]{4}`
