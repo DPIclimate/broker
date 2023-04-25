e3e0ecc2faa43091c9ba8a4808a51a28eb897090
RishCSU <rsing107@postoffice.csu.edu.au>
2023-04-25 10:57:47 +1000

    .json file format

--- 303/elaboration-1/prom/stop.sh
+++ 303/elaboration-1/prom/stop.sh
@@ -3,4 +3,4 @@
 echo 'stopping prometheus...'
 docker stop prometheus > /dev/null
 docker stop json_exporter > /dev/null
-kill -9 `ps | grep python3 | grep -Eo [0-9]{4}`
+kill -9 `ps | grep python3 | grep -Eo [0-9]{4,5}`
