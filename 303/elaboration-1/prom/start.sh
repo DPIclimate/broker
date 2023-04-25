cd3f4865a87e02b0b0a52f073a565c2302008148
RishCSU <rsing107@postoffice.csu.edu.au>
2023-04-25 10:57:47 +1000

    .json file format

--- 303/elaboration-1/prom/start.sh
+++ 303/elaboration-1/prom/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'starting docker container'
-python3 -m http.server &
+python3 -m http.server 8080 &
 docker run -d --rm --name json_exporter -p 7979:7979 -v $PWD/config/config.yml:/config.yml quay.io/prometheuscommunity/json-exporter --config.file=/config.yml 
 docker run -d --rm --name prometheus -p 9090:9090 -v $PWD/config/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus 
 
