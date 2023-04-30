809f3d519e891f10a303b90b5d53b171907f4d1a
Desmodena <sara_mezzomo@csu.edu.au>
2023-04-30 11:01:02 +1000

    work in progress python script for review

---
+++ 303/elaboration-1/influx/influx.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "ITC303"
+url = "http://localhost:8086"
+
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+
+bucket="DPI"
+
+write_api = client.write_api(write_options=SYNCHRONOUS)
+   
+for value in range(5):
+  point = (
+    Point("measurement1")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", value)
+  )
+  write_api.write(bucket=bucket, org="ITC303", record=point)
+  time.sleep(1) # separate points by 1 second
+
+query_api = client.query_api()
+
+query = """from(bucket: "DPI")
+ |> range(start: -10m)
+ |> filter(fn: (r) => r._measurement == "measurement1")"""
+tables = query_api.query(query, org="ITC303")
+
+for table in tables:
+  for record in table.records:
+    print(record)
+
+
