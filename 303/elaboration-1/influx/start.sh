638399eaa283be968512ef222f03ecf3745f2da7
cal <14177650+cufffs@users.noreply.github.com>
2023-04-30 16:14:26 +1000

    added token set up

--- 303/elaboration-1/influx/start.sh
+++ 303/elaboration-1/influx/start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 echo 'starting docker container'
-docker run -d --rm --name influxdb -p 8086:8086  \
--e DOCKER_INFLUXDB_INIT_MODE=setup \
--e DOCKER_INFLUXDB_INIT_USERNAME=Desmodena \
--e DOCKER_INFLUXDB_INIT_PASSWORD=password \
--e DOCKER_INFLUXDB_INIT_ORG=ITC303 \
--e DOCKER_INFLUXDB_INIT_BUCKET=DPI \
-influxdb:2.7.0 >/dev/null
-
-export INFLUXDB_TOKEN=j6OEucJqRI0o-pJLHW6ZtoUBLLCIaP7Aml8NMUp86WIc-kkH56BwJcvZto6TclPpJvxs5rLYiCBR_WLWIKsm5Q==
+docker run -d --rm --name influxdb -p 8086:8086 \
+	-e DOCKER_INFLUXDB_INIT_MODE=setup \
+	-e DOCKER_INFLUXDB_INIT_USERNAME=Desmodena \
+	-e DOCKER_INFLUXDB_INIT_PASSWORD=password \
+	-e DOCKER_INFLUXDB_INIT_ORG=ITC303 \
+	-e DOCKER_INFLUXDB_INIT_BUCKET=DPI \
+	influxdb:2.7.0 >/dev/null
 
 docker ps | grep -q "influxdb" && echo "influxdb running"
