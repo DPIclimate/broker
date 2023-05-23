6153543a3745d5976761bce43957aa050f1317d3
ZakhaevK <zkupke@gmail.com>
2023-05-23 18:13:54 +1000

    Created scripts for sending messages to the rabbitmq queue for testing purposes.

---
+++ timescale/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE timeseries (
+                         broker_id VARCHAR,
+                         l_uid VARCHAR,
+                         p_uid VARCHAR,
+                         timestamp TIMESTAMPTZ NOT NULL,
+                         name VARCHAR,
+                         value VARCHAR
+                         );
