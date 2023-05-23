2073796bd9d337f6690323400dca00b599e6ab52
clare-s <clare_strik@hotmail.com>
2023-05-23 21:16:12 +1000

    Prometheus and Grafana files

--- kubernetes.sh
+++ kubernetes.sh
@@ -4,7 +4,7 @@
 parent_dir=$(dirname "$(pwd)")
 
 # Start minikube
-minikube start
+minikube start 
 
 # Set minikube docker environment
 eval $(minikube -p minikube docker-env)
@@ -14,6 +14,7 @@ docker build -t broker/python-base -f images/restapi/Dockerfile .
 docker build -t broker/ttn_decoder -f images/ttn_decoder/Dockerfile .
 docker build -t broker/mgmt-app -f src/www/Dockerfile .
 
+
 # Mount directories
 minikube mount $parent_dir/broker:/home/broker &
 minikube mount $parent_dir/ttn-formatters:/home/ttn-formatters &
@@ -21,7 +22,9 @@ minikube mount $parent_dir/databolt:/home/databolt &
 
 # Apply kubernetes configuration
 cd kubernetes
+kubectl apply  -f prometheus.yaml 
 kubectl apply -f .
+
 
 # Open minikube dashboard
 minikube dashboard
