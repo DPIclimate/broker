d46692899d892772f28a68f322efaa80635021fa
ZakhaevK <zkupke@gmail.com>
2023-08-02 16:13:54 +1000

    Removal of some legacy code

--- src/python/timescale/Timescale.py
+++ src/python/timescale/Timescale.py
@@ -15,28 +15,6 @@ dbname = "postgres"
 table_name = "timeseries"
 CONNECTION = f"postgres://{username}:{password}@{host}:{port}/{dbname}"
 
-def create_test_table(connection: str = CONNECTION, table: str = table_name):
-    query_create_table = f"""CREATE TABLE {table} (
-                                        broker_id VARCHAR,
-                                        l_uid VARCHAR,
-                                        p_uid VARCHAR,
-                                        timestamp TIMESTAMPTZ NOT NULL,
-                                        name VARCHAR,
-                                        value NUMERIC
-                                    );
-                                    """
-    conn = psycopg2.connect(connection)
-    cursor = conn.cursor()
-    try:
-        cursor.execute(query_create_table)
-        # Creates a hypertable for time-based partitioning
-        cursor.execute(f"SELECT create_hypertable('{table}', 'timestamp');")
-        # commit changes to the database to make changes persistent
-        conn.commit()
-    except psycopg2.errors.DuplicateTable as e:
-        sys.stderr.write(f'error: {e}\n')
-    cursor.close()
-
     # Produce the test temperature data that is inserted.
 def generate_test_message():
     json_example = []
@@ -48,23 +26,6 @@ def generate_test_message():
         json_example.append(json_item)
         return json_example
 
-# def insert_lines(json_entries: list = generate_test_message(), connection: str = CONNECTION,):
-#     conn = psycopg2.connect(connection)
-#     cursor = conn.cursor()
-#     try:
-#         for json_data in json_entries:
-#         # Parse the JSON message and extract the relevant data fields
-#             data = json.loads(json_data)
-#             l_uid = data['l_uid']
-#             p_uid = data['p_uid']
-#             name = data['name']
-#             value = data['value']
-
-#             cursor.execute("INSERT INTO test_table (l_uid, p_uid, timestamp, name, value) VALUES (%s, %s, CURRENT_TIMESTAMP, %s, %s);",
-#                            (l_uid, p_uid, name, value))
-#     except (Exception, psycopg2.Error) as error:
-#         print(error)
-#     conn.commit()
 
 def insert_lines(parsed_data: list, connection: str = CONNECTION, table: str = table_name) -> int:
     conn = psycopg2.connect(connection)
@@ -267,34 +228,6 @@ def parse_json_string(json_string: str) -> list:
         logging.error(f"An error occurred: {str(e)}")
         return []
 
-
-# def parse_json_string(json_string: str) -> list:
-#     parsed_data = []
-#     json_str = ""
-
-#     for line in json_string.split('\n'):
-#         json_str += line.strip()
-#         try:
-#             json_obj = json.loads(json_str)
-#             broker_id = json_obj['broker_correlation_id']
-#             l_uid = json_obj['l_uid']
-#             p_uid = json_obj['p_uid']
-#             timestamp = parser.parse(json_obj['timestamp'])
-#             timeseries = json_obj['timeseries']
-
-#             for tsd in timeseries:
-#                 name = tsd['name']
-#                 value = tsd['value']
-#                 parsed_data.append((broker_id, l_uid, p_uid, timestamp, name, value))
-
-#             json_str = ""
-#         except json.decoder.JSONDecodeError as e:
-#             logging.error(f"An error occurred: {str(e)}")
-#             pass
-
-#     return parsed_data
-
-
 def parse_json_file(filename: str) -> list:
     parsed_data = []
 
@@ -323,7 +256,7 @@ def parse_json_file(filename: str) -> list:
     return parsed_data
 
 
-def insert_data_to_db(filename: str, connection: str = CONNECTION, table_name: str = table_name) -> int:
+def insert_file_to_db(filename: str, connection: str = CONNECTION, table_name: str = table_name) -> int:
     # Parse the JSON file
     parsed_data = parse_json_file(filename)
     conn = psycopg2.connect(connection)
@@ -343,33 +276,4 @@ def insert_data_to_db(filename: str, connection: str = CONNECTION, table_name: s
     return 1
     
 
-test_message = """{
-  "broker_correlation_id": "83d04e6f-db16-4280-8337-53f11b2335c6",
-  "p_uid": 301,
-  "l_uid": 276,
-  "timestamp": "2023-01-30T06:21:56Z",
-  "timeseries": [
-    {
-      "name": "battery (v)",
-      "value": 4.16008997
-    },
-    {
-      "name": "pulse_count",
-      "value": 1
-    },
-    {
-      "name": "1_Temperature",
-      "value": 21.60000038
-    }
-  ]
-}
-"""
-
-if __name__ == "__main__":
-    #create_test_table()
-    #lol = parse_json_string(test_message)
-    #print(lol)
-    #insert_lines(lol)
-    #insert_data_to_db("JSON_message")
-    #insert_data_to_db("sample_messages")
-    print(query_all_data())
+# if __name__ == "__main__":
