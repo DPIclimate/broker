baabd622d078e4ae1e4b5d2003c9f7b03ef94dc4
ZakhaevK <zkupke@gmail.com>
2023-08-05 20:07:27 +1000

    Updated some scripts as image name changed.

--- 303/timescale-testing/clear_database.sh
+++ 303/timescale-testing/clear_database.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec test-zak-python-1 python -c "import timescale.Timescale as ts; ts.remove_data_with_value()"
+docker exec test-iota_tsdb_decoder-1 python -c "import timescale.Timescale as ts; ts.remove_data_with_value()"
--- 303/timescale-testing/test_unit.sh
+++ 303/timescale-testing/test_unit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec test-zak-python-1 pytest -v timescale/test/UnitTest.py
+docker exec test-iota_tsdb_decoder-1 pytest -v timescale/test/UnitTest.py
