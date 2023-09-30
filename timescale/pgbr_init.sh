87c16158faadfb90478ac1a66c6f23541ea7cc61
ZakhaevK <zkupke@gmail.com>
2023-09-30 15:49:45 +1000

    Seems to work up until restore.

--- timescale/pgbr_init.sh
+++ timescale/pgbr_init.sh
@@ -12,10 +12,10 @@ wait_for_postgres() {
 wait_for_postgres
 
 # Run stanza-create (only if the stanza doesn't already exist)
-if ! pgbackrest info --stanza=demo --config=/home/postgres/pgdata/backup/pgbackrest.conf 2>&1 | grep -q "stanza: demo"; then
-    pgbackrest --stanza=demo --config=/home/postgres/pgdata/backup/pgbackrest.conf stanza-create >> /var/log/timescale/pgbr_init.log 2>&1
-fi
+#if ! pgbackrest info --stanza=demo --config=/home/postgres/pgdata/backup/pgbackrest.conf 2>&1 | grep -q "stanza: demo"; then
+#    pgbackrest --stanza=demo --config=/home/postgres/pgdata/backup/pgbackrest.conf stanza-create >> /var/log/timescale/pgbr_init.log 2>&1
+#fi
 
 # Continue with the default TimescaleDB entrypoint
-exec /usr/local/bin/docker-entrypoint.sh
+#exec /usr/local/bin/docker-entrypoint.sh
 
