2cdda21550849a61bb5bc7eefe4a90a699a43d26
Jack Footner <jackrfootner@gmail.com>
2023-10-04 09:43:12 +1030

    Orgianise kubenetes script with functions - add status after start

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-# Get the mode to run in
-if [ "$1" = "status" ]; then
-    echo "Getting all Broker System Resources"
-
-	# Get namespace resources
-	kubectl get all --namespace=broker
-
-elif [ "$1" = "stop" ]; then
-    echo "Stopping Broker System"
-
-	# Delete namespace (and all resources in it)
-	kubectl delete namespace/broker
-
-elif [ "$1" = "restart" ]; then
-    echo "Restarting Broker System"
-
-	# Delete namespace (and all resources in it)
-	kubectl delete namespace/broker
-	
+startBroker () {
 	# Build docker images
 	echo -e "Building Local Docker Images"
 	docker build -q -t broker/python-base -f images/restapi/Dockerfile .
@@ -27,24 +9,37 @@ elif [ "$1" = "restart" ]; then
 
 	# Apply kubernetes configuration
 	# Apply namepsace first so it is available for other resources
+	echo -e "Applying Kubernetes Configurations"
 	kubectl apply -f kubernetes/namespace.yaml
 	kubectl apply -f kubernetes/env-configmap.yaml
 	kubectl apply -f kubernetes/services
 	kubectl apply -f kubernetes/nodeports.yaml
+}
 
+stopBroker () {
+	# Delete namespace (and all resources in it)
+	echo -e "Removing Kubernetes Configurations"
+	kubectl delete namespace/broker
+}
+
+listResources () {
+	# Get namespace resources
+	kubectl get all --namespace=broker
+}
+
+# Get the mode to run in
+if [ "$1" = "status" ]; then
+	echo "Getting all Broker System Resources"
+	listResources
+elif [ "$1" = "stop" ]; then
+	echo "Stopping Broker System"
+	stopBroker
+elif [ "$1" = "restart" ]; then
+	echo "Restarting Broker System"
+	stopBroker
+	startBroker
 else
 	echo "Starting Broker System"
-
-	# Build docker images
-	echo -e "Building Local Docker Images"
-	docker build -q -t broker/python-base -f images/restapi/Dockerfile .
-	docker build -q -t broker/ttn_decoder -f images/ttn_decoder/Dockerfile .
-	docker build -q -t broker/mgmt-app -f src/www/Dockerfile .
-
-	# Apply kubernetes configuration
-	kubectl apply -f kubernetes/namespace.yaml
-	kubectl apply -f kubernetes/env-configmap.yaml
-	kubectl apply -f kubernetes/services
-	kubectl apply -f kubernetes/nodeports.yaml
-
+	startBroker
+	listResources
 fi
