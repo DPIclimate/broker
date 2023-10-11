67c3f7867f115c0a4fb8ed83ef8325e74b94e05f
ZakhaevK <zkupke@gmail.com>
2023-10-11 15:59:29 +1100

    Updated pgbr_restore with warning about postgres timeline, and when to use it.

--- pgbr_restore.sh
+++ pgbr_restore.sh
@@ -3,9 +3,23 @@
 # Load environment variables from .env file
 source compose/.env 2>/dev/null
 
+# Warning prompt about timelines
+echo "WARNING:"
+echo "Postgres makes use of timelines, which keep logs of changes from a point in time, allowing for backups to be restored to a certain point in its history."
+echo
+echo "Due to this, you should only attempt restore to a point prior to your last restore as part of that timeline, otherwise the restore will fail."
+echo
+echo "If you have recently performed a logical backup, the timeline history will be erased, resulting in all backups prior being incompatible with the current database."
+echo
+read -p "Do you wish to continue? (yes/no): " choice
+
+if [[ "$choice" != "yes" ]]; then
+    echo "Exiting restore process."
+    exit 1
+fi
+
 # Find the container name containing "timescale-1"
 DB_CONTAINER_NAME=$(docker ps --format '{{.Names}}' | grep "timescaledb-1")
-
 if [ -z "$DB_CONTAINER_NAME" ]; then
     echo "Error: Container containing 'timescale-1' not found."
     exit 1
@@ -16,6 +30,13 @@ if [ -z "$TSDB_USER" ] || [ -z "$TSDB_DB" ] || [ -z "$TSDB_PASSWORD" ]; then
     echo "Error: Required environment variables are not set."
     exit 1
 fi
+
+# List the backups without starting the temp container
+echo "Available backups:"
+docker exec -t $DB_CONTAINER_NAME pgbackrest info --stanza=demo
+
+# Ask the user for the backup label
+read -p "Enter the backup label to restore (or press Enter for the latest): " BACKUP_LABEL
 
 # Stop the original container
 echo "Stopping the original container..."
@@ -31,18 +52,9 @@ docker run -d \
   -v $(pwd)/timescale/pgbackrest/pgbackrest.conf:/home/postgres/pgdata/backup/pgbackrest.conf \
   custom-timescaledb:latest /bin/sh -c "tail -f /dev/null & wait"
 
-# List the backups
-echo "Available backups:"
-docker exec -t $TEMP_CONTAINER_NAME pgbackrest info --stanza=demo
-
-# Ask the user for the backup label
-read -p "Enter the backup label to restore (or press Enter for the latest): " BACKUP_LABEL
-
 # Ensure the PostgreSQL data directory is empty
 echo "Ensuring the PostgreSQL data directory is empty..."
 docker exec -t $TEMP_CONTAINER_NAME sh -c "rm -rf /home/postgres/pgdata/data/* && rm -rf /home/postgres/pgdata/data/.*"
-
-
 
 # Restore the database on the temporary container
 echo "Restoring the database..."
@@ -51,11 +63,6 @@ if [ -z "$BACKUP_LABEL" ]; then
 else
     docker exec -t $TEMP_CONTAINER_NAME pgbackrest restore --stanza=demo --set=$BACKUP_LABEL
 fi
-
-# Remove any existing WAL files
-#echo "Removing residual WAL files..."
-#docker exec -t $TEMP_CONTAINER_NAME sh -c "rm -rf /home/postgres/pgdata/data/pg_wal/*"
-
 
 # Stop the temporary container
 echo "Stopping the temporary container..."
@@ -69,5 +76,4 @@ sleep 5
 docker exec -it $DB_CONTAINER_NAME psql -U $TSDB_USER -d $TSDB_DB -c "SELECT pg_wal_replay_resume();"
 
 echo "Database restored successfully."
-
 
