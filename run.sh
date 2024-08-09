31c281c2c9bdaf5195302ea15102c8c1a8d73666
David Taylor <david.a.taylor@dpi.nsw.gov.au>
2024-08-09 10:41:23 +1000

    Now creating a versioned image.

--- run.sh
+++ run.sh
@@ -13,9 +13,15 @@ cd $BROKER_ROOT
 cd compose/$MODE
 ./dc.sh down
 cd $BROKER_ROOT
+echo Building REST API image
 docker build -q -t broker/python-base -f images/restapi/Dockerfile .
+echo Building TTN decoder image
 docker build -q -t broker/ttn_decoder -f images/ttn_decoder/Dockerfile .
+echo Building web app image
 docker build -q -t broker/mgmt-app -f src/www/Dockerfile .
-cd compose/$MODE
+echo Building QGIS server image
+cd images/qgis-server
+docker build -q -f Dockerfile -t qgis-server:3.38.1 ./
+cd ../../compose/$MODE
 ./dc.sh up -d
 ./dc.sh logs -f
